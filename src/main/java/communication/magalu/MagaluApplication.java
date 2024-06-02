c116771d7078fa1e7303263cefcd32b68d19522c
btoledo90 <brunotq19@hotmail.com>
2024-06-02 19:14:12 -0300

    Initial commit

---
+++ src/main/java/communication/magalu/MagaluApplication.java
@@ -0,0 +1,13 @@
+package communication.magalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/communication/magalu/MagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package communication.magalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
